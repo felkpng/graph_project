40d00aa044268a66d65e83ae68a713d8a7e6e7d6
felkpng <kavardakfix228@gmail.com>
2025-11-24 18:43:46 +0300

    Алгоритм DFS

--- methods.h
+++ methods.h
@@ -7,13 +7,37 @@
 using namespace std;
 namespace fs = std::filesystem;
 
-bool dfs_method(Graph& graph) {}
+void DFS(string i, map<string, vector<string>>& adjacency, map<string, bool>& visited) {
+    visited[i] = true;
+    for (string u : adjacency[i]) {
+        if (!visited[u])
+            DFS(u, adjacency, visited);
+    }
+}
 
-bool bfs_method(Graph& graph) {}
+bool dfs_method(Graph& graph) {
+    map<string, vector<string>> adjacency = graph.getAdjacency();
+    map<string, bool> visited;
+    graph.get_keys();
 
-bool dsu_method(Graph& graph) {}
+    for (const auto& k : graph.keys)
+        visited[k] = false;
 
-bool floydWarshall_method(Graph& graph) {}
+    string start = graph.keys[0];
+
+    DFS(start, adjacency, visited);
+
+    for (const auto& pair : visited)
+        if (!pair.second)
+            return false;
+    return true;
+}
+
+bool bfs_method(Graph& graph) { return true; }
+
+bool dsu_method(Graph& graph) { return true; }
+
+bool floydWarshall_method(Graph& graph) { return true; }
 
 void LoadData(vector<Graph>& graphs) {
     string directory_path = "./data/";
@@ -33,16 +57,16 @@ void LoadData(vector<Graph>& graphs) {
 void tester() {
     vector<Graph> graphs;
     LoadData(graphs);
-    cout << "Загруженно " << graphs.size() << " графов\n";
+    cout << "Загружено " << graphs.size() << " графов\n";
 
     for (int i = 0; i < graphs.size(); i++) {
-        //const auto& dfs_result = dfs_method(graphs[i]);
+        const auto& dfs_result = dfs_method(graphs[i]);
         //const auto& bfs_result = bfs_method(graphs[i]);
         //const auto& dsu_result = dsu_method(graphs[i]);
         //const auto& floyd_result = floydWarshall_method(graphs[i]);
 
         cout << "Graph" << i + 1 << endl;
-        //cout << "DFS:           " << (dfs_result ? "связанный" : "не связанный") << endl;
+        cout << "DFS:           " << (dfs_result ? "связанный" : "не связанный") << endl;
         //cout << "BFS:           " << (bfs_result ? "связанный" : "не связанный") << endl;
         //cout << "DSU:           " << (dsu_result ? "связанный" : "не связанный") << endl;
         //cout << "floydWarshall: " << (floyd_result ? "связанный" : "не связанный") << endl << endl;
