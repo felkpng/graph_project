303aca826e8afcfe7491a2d14fea80fded403314
sophix-wh <psofia2312@mail.ru>
2025-11-30 21:17:56 +0300

    реализован dfs метод

--- methods.h
+++ methods.h
@@ -7,7 +7,31 @@
 using namespace std;
 namespace fs = std::filesystem;
 
-bool dfs_method(Graph& graph) { return true; }
+void DFS(string i, map<string, vector<string>>& adjacency, map<string, bool>& visited) {
+    visited[i] = true;
+    for (string u : adjacency[i]) {
+        if (!visited[u])
+            DFS(u, adjacency, visited);
+    }
+}
+
+bool dfs_method(Graph& graph) {
+    map<string, vector<string>> adjacency = graph.getAdjacency();
+    map<string, bool> visited;
+    graph.get_keys();
+
+    for (const auto& k : graph.keys)
+        visited[k] = false;
+
+    string start = graph.keys[0];
+
+    DFS(start, adjacency, visited);
+
+    for (const auto& pair : visited)
+        if (!pair.second)
+            return false;
+    return true;
+}
 
 bool bfs_method(Graph& graph) { return true; }
 
@@ -36,13 +60,13 @@ void tester() {
     cout << "Загруженно " << graphs.size() << " графов\n\n";
 
     for (int i = 0; i < graphs.size(); i++) {
-        //const auto& dfs_result = dfs_method(graphs[i]);
+        const auto& dfs_result = dfs_method(graphs[i]);
         //const auto& bfs_result = bfs_method(graphs[i]);
         //const auto& dsu_result = dsu_method(graphs[i]);
         //const auto& floyd_result = floydWarshall_method(graphs[i]);
 
         cout << "Graph" << i + 1 << endl;
-        //cout << "DFS:           " << (dfs_result ? "связанный" : "не связанный") << endl;
+        cout << "DFS:           " << (dfs_result ? "связанный" : "не связанный") << endl;
         //cout << "BFS:           " << (bfs_result ? "связанный" : "не связанный") << endl;
         //cout << "DSU:           " << (dsu_result ? "связанный" : "не связанный") << endl;
         //cout << "FloydWarshall: " << (floyd_result ? "связанный" : "не связанный") << endl << endl;
